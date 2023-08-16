74160f9c6a19f43e10241c3d4373dde0dc41cd0b
JaeMin1130 <ija06598@gmail.com>
2023-08-16 22:46:42 +0900

    08/16

--- front/src/api/api-config.js
+++
@@ -1,8 +0,0 @@
-let backendHost;
-
-const hostname = window && window.location && window.location.hostname;
-
-backendHost = "http://localhost:8080";
-// backendHost = "http://10.125.121.173:8080";
-
-export const API_BASE_URL = `${backendHost}`;
